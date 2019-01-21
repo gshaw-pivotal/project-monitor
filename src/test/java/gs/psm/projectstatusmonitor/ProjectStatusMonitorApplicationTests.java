6346c25256bc0b193ede00784332d0d4c51ba9a8
gshaw-pivotal <gshaw+github@pivotal.io>
2019-01-20 23:43:54 -0500

    Add full application test for project update endpoint
    - Stand up full application (in test profile) and check that a user not
      associated with a project is unable to update it.

--- src/test/java/gs/psm/projectstatusmonitor/ProjectStatusMonitorApplicationTests.java
+++ src/test/java/gs/psm/projectstatusmonitor/ProjectStatusMonitorApplicationTests.java
@@ -1,16 +1,56 @@
 package gs.psm.projectstatusmonitor;
 
+import gs.psm.projectstatusmonitor.models.Project;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProjectStatusMonitorApplicationTests {
+
+	@LocalServerPort
+	private int serverPort;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void updateProject_whenTheUserIsNotAssociatedWithTheProject_receives400Response() {
+		Project project = createProject(1);
+		ResponseEntity response;
+
+		response = restTemplate
+				.withBasicAuth("test_username", "test_password")
+				.postForEntity("http://localhost:" + serverPort + "/project/add", project, Void.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		project.setProjectName("Name name");
+		response = restTemplate
+				.withBasicAuth("test_username2", "test_password2")
+				.postForEntity("http://localhost:" + serverPort + "/project/update", project, Void.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+	}
+
+	private Project createProject(int increment) {
+		return Project.builder()
+				.projectCode("code" + increment)
+				.projectName("name" + increment)
+				.jobStatusList(null)
+				.build();
+	}
 }
